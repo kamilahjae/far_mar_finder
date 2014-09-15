37f61f2b2f088e4b154033877c8fe621bb0eaedc
KamilahJ4e <kamilahjae@gmail.com>
2014-09-15 16:16:26 -0700

    changed csv.open to csv.read, set market id to id instance variable.

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -3,19 +3,19 @@ module FarMar
     attr_accessor :id
 
     def initialize(market)
-      @id = id
+      @id = market[0]
       @market = market
     end
 
     def self.all
-      @market_array = CSV.open("./support/markets.csv", "r")
+      @market_array = CSV.read("./support/markets.csv", "r")
     end
 
     # Goal: return the row where the ID field matches the argument
     def self.find(id)
       # 1 Feeds in ID as parameter
       # 2 Match the given ID with the Market's ID
-      row_id = @market_array.find {|row| row[0] == id }
+      @market_array.find {|row| row[0] == id }
       # Row is returned implicitly
     end
 
@@ -24,6 +24,8 @@ module FarMar
       @vendor_array = CSV.open("./support/vendors.csv", "r")
       market_id = @vendor_array
     end
-    
+
   end
 end
+
+m = Market.new
