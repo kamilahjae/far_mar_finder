2351c30ccc6a5526402dcc40cb7f35c613f3f773
Rebecca Mark <rebecca.lmark@gmail.com>
2014-09-16 14:55:36 -0700

    Working self.by_market function

--- lib/far_mar/vendor.rb
+++ lib/far_mar/vendor.rb
@@ -24,7 +24,7 @@ module FarMar
     # Return all of the vendors with the given market id
     def self.by_market(market_id)
       vendors = FarMar::Vendor.all
-      vendors.find_all {|vendor| vendor.market_id == self.id}
+      vendors.find_all {|vendor| vendor.market_id == market_id}
     end
 
     # Return the Market instance associated with this vendor using the
