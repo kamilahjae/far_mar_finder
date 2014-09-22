06ae9bab2c878a443ba3d120fe696469f24e5941
Rebecca Mark <rebecca.lmark@gmail.com>
2014-09-22 13:14:21 -0700

    Created 2 benchmark tests for market class under far_mar.rb.

--- lib/far_mar.rb
+++ lib/far_mar.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'time'
 require 'date'
+require 'benchmark'
 
 # This file is to require all of our dependencies (each of the classes we make)
 require_relative 'far_mar/market'
@@ -8,3 +9,11 @@ require_relative 'far_mar/vendor'
 require_relative 'far_mar/product'
 require_relative 'far_mar/sale'
 # to load in irb do require_relative 'far_mar/market'
+
+### Benchmarking Far_mar Market
+m = FarMar::Market.find(1)
+
+Benchmark.bm do |x|
+  x.report { m.preferred_vendor(2013,11,11)}
+  x.report { FarMar::Market.find(90)}
+end
